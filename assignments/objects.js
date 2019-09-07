9cf911f4fc34082baadbcb081fc9d41d87e6ff13
Bonnie Turnbeaugh <turnbeaugh@comcast.net>
2019-09-07 10:11:41 -0700

    fixed error in objects.js

--- assignments/objects.js
+++ assignments/objects.js
@@ -78,10 +78,10 @@ console.log(intern.intern5.gender);
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
-console.log(intern2.speak);
+console.log(intern.intern2.speak);
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
-console.log(intern5.multiplyNums(num1, num2));
+console.log(intern.intern5.multiplyNums(3, 4));
 // === Great work! === Head over to the the arrays.js. You may come back and attempt the Stretch Challenge once you have completed the challenges in arrays.js and function-conversion.js.
 
 // ==== Stretch Challenge: Nested Objects and the this keyword ==== 
@@ -90,17 +90,3 @@ console.log(intern5.multiplyNums(num1, num2));
 // 2. Nest a child object in the parent object with name and age as well.  The name will be George and the age will be 50.
 // 3. Nest a grandchild object in the child object with properties for name and age.  The name will be Sam and the age will be 30
 // 4. Give each of the objects the ability to speak their names using the this keyword.
-
-const parent = {}
-
-// Log the parent object's name
-
-// Log the child's age
-
-// Log the name and age of the grandchild
-
-// Have the parent speak
-
-// Have the child speak
-
-// Have the grandchild speak
